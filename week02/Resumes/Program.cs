2eae25beb0bccd4b3d40540f4a808e145c81f117
Nathaniel-Stokes35 <Nathaniel.Stokes35@gmail.com>
2025-03-12 20:13:38 -0500

    Week 2 Learning Activity

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job
+        {
+            _jobTitle = "Software Engineer",
+            _company = "Microsoft",
+            _startYear = 2017,
+            _endYear = 2020
+        };
+        Job job2 = new Job
+        {
+            _jobTitle = "Software Engineer",
+            _company = "Apple",
+            _startYear = 2020,
+            _endYear = 2021
+        };
+        Resume resume = new Resume
+        {
+            _name = "Nathaniel Stokes"
+        };
+
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+        resume.Display();
     }
 }
